52fa0d3dd59f7892b1baf3f4f3dbca3e02df9bcf
marcelooliveira <mclricardo@gmail.com>
2026-05-06 13:47:54 -0300

    feat: Sliding Window

---
+++ 01_LeetCode_Marcelo/01.SlidingWindow/0209-MinimumSizeSubarraySum.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------------
+// Runtime: 100ms
+// Memory Usage: 25.2 MB
+// Link: https://leetcode.com/submissions/detail/372037277/
+//-----------------------------------------------------------------------------
+
+using System;
+
+namespace LeetCode
+{
+    public class _0209_MinimumSizeSubarraySum
+    {
+		/*
+         Janela dinâmica que encolhe assim que atinge a soma alvo.
+
+            1. Expanda a janela somando elementos com o ponteiro right.
+            2. Enquanto a soma for ≥ alvo, registre o menor 
+                    comprimento e encolha a janela movendo left para a direita.
+         */
+		public int MinSubArrayLen(int s, int[] nums)
+        {
+            var result = int.MaxValue;
+            var sum = 0;
+            var left = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                while (sum >= s)
+                {
+                    result = Math.Min(result, i - left + 1);
+                    sum -= nums[left++];
+                }
+            }
+
+            return result == int.MaxValue ? 0 : result;
+        }
+    }
+}
---
+++ 01_LeetCode_Marcelo/01.SlidingWindow/053-MaximumSubarray.cs
@@ -0,0 +1,34 @@
+////-----------------------------------------------------------------------------
+//// Runtime: 88ms
+//// Memory Usage: 25 MB
+//// Link: https://leetcode.com/submissions/detail/320345171/
+////-----------------------------------------------------------------------------
+
+//using System;
+
+//namespace LeetCode
+//{
+//    public class _053_MaximumSubarray
+//    {
+//        /*
+//	        Manter a soma atual e redefinir se ela se tornar negativa.
+
+//            1. Varra o array acumulando a soma em currentSum.
+//            2. Se currentSum > maxSum, atualize maxSum.
+//            3. Se currentSum < 0, resete currentSum = 0.
+//         */
+//        public int MaxSubArray(int[] nums)
+//        {
+//            if (nums.Length == 0) return int.MinValue;
+//            int currSum = nums[0];
+//            int maxSum = nums[0];
+
+//            for (int i = 1; i < nums.Length; i++)
+//            {
+//                currSum = Math.Max(nums[i], currSum + nums[i]);
+//                maxSum = Math.Max(maxSum, currSum);
+//            }
+//            return maxSum;
+//        }
+//    }
+//}
